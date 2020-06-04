8a2002891cd3f5b0cda785839bd1ebef92f23c5a
Anton Uramer <Anton Uramer>
2020-06-04 23:38:23 +0200

    Use coroutines instead of callbacks

--- main.lua
+++ main.lua
@@ -8,123 +8,95 @@ local TYPES = tableHelper.enum({
     "ListBox"
 })
 
-GuiFramework.nameCache = {}
-GuiFramework.typeCache = {}
-GuiFramework.callbackCache = {}
-GuiFramework.returnCache = {}
-GuiFramework.parametersCache = {}
+GuiFramework.currentId = 0
+GuiFramework.coroutines = {}
+GuiFramework.MessageBoxGuiId = 'GuiFramework_MessageBox'
 
 function GuiFramework.getGuiId(name)
+    if not name then
+        GuiFramework.currentId = GuiFramework.currentId + 1
+        name = 'GuiFramework' .. tostring(GuiFramework.currentId)
+    end
     local id = guiHelper.ID[name]
     if id ~= nil then
         return id
     end
-
+    
     table.insert(guiHelper.names, name)
     guiHelper.ID = tableHelper.enum(guiHelper.names)
     return guiHelper.ID[name]
 end
 
---pid, name, label
-function GuiFramework.MessageBox(opt)
-    local id = GuiFramework.getGuiId(opt.name)
-    GuiFramework.nameCache[id] = opt.name
-    GuiFramework.typeCache[id] = TYPES.MessageBox
-
-    local label = opt.label or ''
-
-    tes3mp.MessageBox( opt.pid, id, label )
+function GuiFramework.SetCoroutine(id)
+    local co = coroutine.running()
+    if not co then
+        error('[GuiFramework] Must run inside a coroutine!')
+    end
+    if id then
+        GuiFramework.coroutines[id] = co
+    end
+    return co
 end
 
---pid, name, buttons, [label, callback, returnValues, parameters]
-function GuiFramework.CustomMessageBox(opt)
-    local id = GuiFramework.getGuiId(opt.name)
-    GuiFramework.nameCache[id] = opt.name
-    GuiFramework.typeCache[id] = TYPES.CustomMessageBox
-    GuiFramework.callbackCache[id] = opt.callback
-
-    GuiFramework.returnCache[opt.pid] = opt.returnValues or opt.buttons
-    GuiFramework.parametersCache[opt.pid] = opt.parameters
-
-    local label = opt.label or ''
-
-    tes3mp.CustomMessageBox( opt.pid, id, label, table.concat(opt.buttons, ";") )
+function GuiFramework.MessageBox(pid, label)
+    local id = GuiFramework.getGuiId(GuiFramework.MessageBoxGuiId)
+    tes3mp.MessageBox(pid, id, label)
+    return true
 end
 
---pid, name, [label, note, callback, parameters]
-function GuiFramework.InputDialog(opt)
-    local id = GuiFramework.getGuiId(opt.name)
-    GuiFramework.nameCache[id] = opt.name
-    GuiFramework.typeCache[id] = TYPES.InputDialog
-    GuiFramework.callbackCache[id] = opt.callback
-
-    GuiFramework.parametersCache[opt.pid] = opt.parameters
-
-    local note = opt.note or ''
-    local label = opt.label or ''
-
-    tes3mp.InputDialog( opt.pid, id, label, note )
+--pid, buttons, [label]
+function GuiFramework.CustomMessageBox(pid, buttons, label)
+    local id = GuiFramework.getGuiId()
+    GuiFramework.SetCoroutine(id)
+    label = label or ''
+    tes3mp.CustomMessageBox(pid, id, label, table.concat(buttons, ";"))
+    return coroutine.yield() + 1
 end
 
---pid, name, [label, note, callback, parameters]
-function GuiFramework.PasswordDialog(opt)
-    local id = GuiFramework.getGuiId(opt.name)
-    GuiFramework.nameCache[id] = opt.name
-    GuiFramework.typeCache[id] = TYPES.PasswordDialog
-    GuiFramework.callbackCache[id] = opt.callback
-
-    GuiFramework.parametersCache[opt.pid] = opt.parameters
-
-    local note = opt.note or ''
-    local label = opt.label or ''
-
-    tes3mp.PasswordDialog( opt.pid, id, label, note )
+--pid, [label, note]
+function GuiFramework.InputDialog(pid, label, note)
+    local id = GuiFramework.getGuiId()
+    GuiFramework.SetCoroutine(id)
+    note = note or ''
+    label = label or ''
+    tes3mp.InputDialog(pid, id, label, note)
+    local result = coroutine.yield()
+    return result
 end
 
---pid, name, rows, [label, callback, returnValues, parameters]
-function GuiFramework.ListBox(opt)
-    local id = GuiFramework.getGuiId(opt.name)
-    GuiFramework.nameCache[id] = opt.name
-    GuiFramework.typeCache[id] = TYPES.ListBox
-    GuiFramework.callbackCache[id] = opt.callback
+--pid, [label, note]
+function GuiFramework.PasswordDialog(pid, label, note)
+    local id = GuiFramework.getGuiId()
+    GuiFramework.SetCoroutine(id)
+    label = label or ''
+    note = note or ''
+    tes3mp.PasswordDialog(pid, id, label, note)
+    return coroutine.yield()
+end
 
-    GuiFramework.returnCache[opt.pid] = opt.returnValues or opt.rows
-    GuiFramework.parametersCache[opt.pid] = opt.parameters
-
-    local label = opt.label or ''
-    local items = table.concat(opt.rows,"\n")
-
-    tes3mp.ListBox( opt.pid, id, label, items )
+--pid, rows, [label]
+function GuiFramework.ListBox(pid, rows, label)
+    local id = GuiFramework.getGuiId()
+    GuiFramework.SetCoroutine(id)
+    local items = table.concat(rows, "\n")
+    label = label or ''
+    tes3mp.ListBox(pid, id, label, items)
+    local result = coroutine.yield() + 1
+    if result > #rows or result < 1 then
+        return nil
+    end
+    return result
 end
 
 function GuiFramework.OnGUIAction(evenStatus, pid, id, data)
-    local callback = GuiFramework.callbackCache[id]
-
-    if callback ~= nil then
-        local name = GuiFramework.nameCache[id]
-        local type = GuiFramework.typeCache[id]
-
-        if type == TYPES.CustomMessageBox then
-            local input = tonumber(data) + 1
-            local value = GuiFramework.returnCache[pid][input]
-
-            callback(pid, name, input, value, GuiFramework.parametersCache[pid])
-
-        elseif type == TYPES.InputDialog then
-            callback(pid, name, data, GuiFramework.parametersCache[pid])
-
-        elseif type == TYPES.PasswordDialog then
-            callback(pid, name, data, GuiFramework.parametersCache[pid])
-
-        elseif type == TYPES.ListBox then
-            local input = tonumber(data) + 1
-            local value = GuiFramework.returnCache[pid][input]
-
-            callback(pid, name, input, value, GuiFramework.parametersCache[pid])
+    if evenStatus.validCustomHandlers then
+        local co = GuiFramework.coroutines[id]
+        if co then
+            GuiFramework.coroutines[id] = nil
+            coroutine.resume(co, data)
         end
     end
 end
-
 customEventHooks.registerHandler("OnGUIAction", GuiFramework.OnGUIAction)
 
 return GuiFramework
